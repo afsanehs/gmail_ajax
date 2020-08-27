9cda5d658693070d49ccf260e4c0e784ed21985b
afsanehs <afsaneh.samettehrani@essec.edu>
2020-08-27 23:42:43 +0200

    -

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -1,5 +1,14 @@
 class EmailController < ApplicationController
     def index 
         @emails = Email.all
+    end
+
+    def create 
+        @email = Email.create(email_params)
+
+        respond_to do |format|
+            format.html {}
+            format.js {}
+        end 
     end 
 end

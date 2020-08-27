67cde8ad7368c93fded81ff43cc471c374130bf7
afsanehs <afsaneh.samettehrani@essec.edu>
2020-08-27 23:29:51 +0200

    -

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "starting seeds"
+
+email1 = Email.create!(objet:"Blablabla", body:"Bonjour, Blabla blaaaaa.")
+email2 = Email.create!(objet:"Inviation de Mariage", body:"Vous êtes invité au marriage de Julia & Paul au chateau de Vincennes")
+email3 = Email.create!(objet:"Facture FREEBOX", body:"PDF généré automatiquement")
+email4 = Email.create!(objet:"hellooooo", body:"çaaaaa vaaaaaaaa ??????")
+email5 = Email.create!(objet:"Bonjour José", body:"Bonjour José, Veuillez cliquer ici pour reçevoir votre cadeau")
+email6 = Email.create!(objet:"Bjfdkjfkd", body:"jkfjfk fd f,dfdfjkdf.")
+email7 = Email.create!(objet:"Entretien de motivation", body:"M.José, nous vous confirmon votre entretien le 20/10/2020 à 9h00 dans nos locaux")
+email8 = Email.create!(objet:"Euro millions", body:"Vous avez gagné !!!! ")
+email9 = Email.create!(objet:"facture EDF", body:"Bonjour, veuillez trouver en pièce jointe votre facture.")
+email10 = Email.create!(objet:"rdv installation box", body:"Bonjour José, votre freebox sera installé le 10/01/2021")
+
+
+
+puts "done with seeds"
